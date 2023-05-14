91483ddd6d3699c739adee59b9132f79e9b24fce
thilini.yashodha.demel <thilini.yashodha.demel@gmail.com>
2023-05-14 10:58:40 +0530

    New

--- public/js/admin_dashboard.js
+++
@@ -1,5 +0,0 @@
-function makeActive() {
-    var element = document.getElementById("text");
-    element.classList.add("active");
-    element.innerHTML="This is Active";
- }
--- public/js/hospital_popup.js
+++
@@ -1,9 +0,0 @@
-let popup=document.getElementById("popup");
-
-function openPopup(){
-    popup.classList.add("open-popup");
-}
-
-function deleteRecord(){
-    popup.classList.remove("open-popup");
-}
